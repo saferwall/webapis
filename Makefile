d3ed53cceb7de233af19a2eae42bc5a3291231f9
LordNoteworthy <lord.noteworthy@protonmail.com>
2021-03-25 11:16:09 +1100

    adapt dockerfiles to new own repo

---
+++ Makefile
@@ -0,0 +1,57 @@
+# Needed SHELL since I'm using zsh
+SHELL := /bin/bash
+
+# HELP
+# This will output the help for each task
+# thanks to https://marmelab.com/blog/2016/02/29/auto-documented-makefile.html
+.PHONY: help
+
+help: ## This help.
+	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+
+.DEFAULT_GOAL := help
+
+# Retrieve the root directory of the project.
+ROOT_DIR	:=$(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+
+# Define standard colors
+BLACK        := $(shell tput -Txterm setaf 0)
+RED          := $(shell tput -Txterm setaf 1)
+GREEN        := $(shell tput -Txterm setaf 2)
+YELLOW       := $(shell tput -Txterm setaf 3)
+LIGHTPURPLE  := $(shell tput -Txterm setaf 4)
+PURPLE       := $(shell tput -Txterm setaf 5)
+BLUE         := $(shell tput -Txterm setaf 6)
+WHITE        := $(shell tput -Txterm setaf 7)
+
+RESET := $(shell tput -Txterm sgr0)
+
+# Our config file.
+include $(ROOT_DIR)/.env
+
+# Include our internals makefiles.
+include build/docker.mk
+
+
+dk-run:		## Run the docker container
+	sudo docker run -it -p 80:80 --name ml saferwall/$(DOCKER_HUB_IMG)
+
+dk-build: ## Build frontend in a docker container
+	@echo "${GREEN} [*] =============== Build Saferwall ML Pipeline =============== ${RESET}"
+	sudo make docker-build IMG=$(DOCKER_HUB_IMG) \
+		DOCKER_FILE=Dockerfile DOCKER_DIR=. ;
+	@EXIT_CODE=$$?
+	@if test $$EXIT_CODE ! 0; then \
+		sudo make docker-build IMG=$(DOCKER_HUB_IMG) \
+			DOCKER_FILE=Dockerfile DOCKER_DIR=. ; \
+	fi
+
+dk-release: ## Build and release frontend in a docker container.
+	@echo "${GREEN} [*] =============== Build and Release Frontend =============== ${RESET}"
+	sudo make docker-release IMG=$(DOCKER_HUB_IMG) VERSION=$(SAFERWALL_VER) \
+		DOCKER_FILE=Dockerfile DOCKER_DIR=. ;
+	@EXIT_CODE=$$?
+	@if test $$EXIT_CODE ! 0; then \
+		sudo make docker-release IMG=$(DOCKER_HUB_IMG) VERSION=$(SAFERWALL_VER) \
+			DOCKER_FILE=Dockerfile DOCKER_DIR=. ; \
+	fi
---
+++ build/docker.mk
@@ -0,0 +1,97 @@
+REPO = saferwall
+
+docker-build: ## Build the container
+	@docker build $(ARGS) -t $(REPO)/$(IMG) \
+		-f $(DOCKER_FILE) $(DOCKER_DIR)
+
+docker-build-nc: ## Build the container without caching
+	@docker build ${ARGS} --no-cache -t $(REPO)/$(IMG) \
+		-f $(DOCKER_FILE) $(DOCKER_DIR)
+
+docker-run: ## Run container on port configured in `config.env`
+	docker run -d -p 50051:50051 $(REPO)/$(IMG)
+
+docker-up: build run ## Run container
+
+docker-stop: ## Stop and remove a running container
+	docker stop $(IMG); docker rm $(REPO)/$(IMG)
+
+docker-release: docker-repo-login docker-build-nc docker-publish ## Make a release by building and publishing the `{version}` and `latest` tagged containers to ECR
+
+docker-publish: docker-repo-login docker-publish-latest docker-publish-version ## Publish the `{version}` and `latest` tagged containers to ECR
+
+docker-publish-latest: docker-tag-latest ## Publish the `latest` taged container to ECR
+	@echo 'publish latest to $(REPO)/$(IMG)'
+	docker push $(REPO)/$(IMG):latest
+
+docker-publish-version: docker-tag-version ## Publish the `{version}` taged container to ECR
+	@echo 'publish $(VERSION) to $(IMG)'
+	docker push $(REPO)/$(IMG):$(VERSION)
+
+docker-tag: docker-tag-latest docker-tag-version ## Generate container tags for the `{version}` and `latest` tags
+
+docker-tag-latest: 	## Generate container `{version}` tag
+	@echo 'create tag latest'
+	docker tag $(REPO)/$(IMG) $(REPO)/$(IMG):latest
+
+docker-tag-version: 	## Generate container `latest` tag
+	@echo 'create tag $(VERSION)'
+	docker tag $(REPO)/$(IMG) $(REPO)/$(IMG):$(VERSION)
+
+docker-repo-login: 	## Login to Docker Hub
+	@echo '$(DOCKER_HUB_PWD)' | docker login --username=$(DOCKER_HUB_USR) --password-stdin
+
+docker-install:		## install docker
+	sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	sudo apt-key fingerprint 0EBFCD88
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $$(lsb_release -cs) stable"
+	sudo apt-get update
+	sudo apt-get install docker-ce -y
+
+docker-stop-all:		## Stop all containers
+	docker stop $$(docker ps -a -q)
+
+docker-rm-all:			## Delete all containers
+	docker rm $$(docker ps -a -q)
+
+docker-rm-images:		## Delete all images
+	docker rmi $$(docker images -q) -f
+
+docker-rm-dangling:		## Delete all dangling images
+	docker images --quiet --filter=dangling=true | xargs --no-run-if-empty docker rmi -f
+
+docker-rm-image-tags:	## Delete all tags from image, {IMG} as argument.
+	docker images | grep $(IMG) | tr -s ' ' | cut -d ' ' -f 2 | xargs -I {} docker rmi -f $(REPO)/$(IMG):{}
+
+docker-get-ip:			## Get container IP addr
+	docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(IMG)
+
+docker-daemon-restart:	## Restart docker daemon & reload config
+	sudo systemctl daemon-reload
+	sudo systemctl restart docker
+
+docker-enable-experimental:		## Enable experimental
+	echo '{"experimental":true}' >> /etc/docker/daemon.json
+
+docker-stats: ## Get docker stats nicely formatted.
+	docker stats --format "table {{.Container}}\t{{.CPUPerc}}\t{{.MemUsage}}"
+
+docker-non-root: ## Run docker as non root.
+	sudo groupadd docker
+	sudo usermod -aG docker $(USER)
+	newgrp docker 
+	sudo chown "$(USER)":"$(USER)" /home/"$(USER)"/.docker -R
+	sudo chmod g+rwx "$(HOME)/.docker" -R
+
+docker-cadvisor:	## Run docker cAdvisor.
+	docker run \
+	--volume=/:/rootfs:ro \
+	--volume=/var/run:/var/run:rw \
+	--volume=/sys:/sys:ro \
+	--volume=/var/lib/docker/:/var/lib/docker:ro \
+	--volume=/dev/disk/:/dev/disk:ro \
+	--publish=8080:8080 \
+	--detach=true \
+	--name=cadvisor \
+	google/cadvisor:latest
