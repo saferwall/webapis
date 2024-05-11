36c5c048431af24679df62c04415993a6c7e7a1e
LordNoteworthy <lord.noteworthy@protonmail.com>
2024-05-11 21:52:43 +1000

    feat(behavior): include capabilities in file resource

--- internal/entity/file.go
+++ internal/entity/file.go
@@ -10,34 +10,34 @@ import (
 
 // File represent a binary file.
 type File struct {
-	Type              string                 `json:"type,omitempty"`
-	MD5               string                 `json:"md5,omitempty"`
-	SHA1              string                 `json:"sha1,omitempty"`
-	SHA256            string                 `json:"sha256,omitempty"`
-	SHA512            string                 `json:"sha512,omitempty"`
-	SSDeep            string                 `json:"ssdeep,omitempty"`
-	Crc32             string                 `json:"crc32,omitempty"`
-	Size              int64                  `json:"size,omitempty"`
-	Tags              map[string]interface{} `json:"tags,omitempty"`
-	Magic             string                 `json:"magic,omitempty"`
-	Exif              map[string]string      `json:"exif,omitempty"`
-	TriD              []string               `json:"trid,omitempty"`
-	Packer            []string               `json:"packer,omitempty"`
-	FirstSeen         int64                  `json:"first_seen,omitempty"`
-	LastScanned       int64                  `json:"last_scanned,omitempty"`
-	Submissions       []Submission           `json:"submissions,omitempty"`
-	Strings           interface{}            `json:"strings,omitempty"`
-	MultiAV           map[string]interface{} `json:"multiav,omitempty"`
-	PE                interface{}            `json:"pe,omitempty"`
-	Histogram         []int                  `json:"histogram,omitempty"`
-	ByteEntropy       []int                  `json:"byte_entropy,omitempty"`
-	Ml                map[string]interface{} `json:"ml,omitempty"`
-	CommentsCount     *int                   `json:"comments_count,omitempty"`
-	Format            string                 `json:"file_format,omitempty"`
-	Extension         string                 `json:"file_extension,omitempty"`
-	DefaultBehaviorID string                 `json:"default_behavior_id,omitempty"`
-	Behaviors         interface{}            `json:"behaviors,omitempty"`
-	Status            int                    `json:"status,omitempty"`
+	Type             string                 `json:"type,omitempty"`
+	MD5              string                 `json:"md5,omitempty"`
+	SHA1             string                 `json:"sha1,omitempty"`
+	SHA256           string                 `json:"sha256,omitempty"`
+	SHA512           string                 `json:"sha512,omitempty"`
+	SSDeep           string                 `json:"ssdeep,omitempty"`
+	Crc32            string                 `json:"crc32,omitempty"`
+	Size             int64                  `json:"size,omitempty"`
+	Tags             map[string]interface{} `json:"tags,omitempty"`
+	Magic            string                 `json:"magic,omitempty"`
+	Exif             map[string]string      `json:"exif,omitempty"`
+	TriD             []string               `json:"trid,omitempty"`
+	Packer           []string               `json:"packer,omitempty"`
+	FirstSeen        int64                  `json:"first_seen,omitempty"`
+	LastScanned      int64                  `json:"last_scanned,omitempty"`
+	Submissions      []Submission           `json:"submissions,omitempty"`
+	Strings          interface{}            `json:"strings,omitempty"`
+	MultiAV          map[string]interface{} `json:"multiav,omitempty"`
+	PE               interface{}            `json:"pe,omitempty"`
+	Histogram        []int                  `json:"histogram,omitempty"`
+	ByteEntropy      []int                  `json:"byte_entropy,omitempty"`
+	Ml               map[string]interface{} `json:"ml,omitempty"`
+	CommentsCount    *int                   `json:"comments_count,omitempty"`
+	Format           string                 `json:"file_format,omitempty"`
+	Extension        string                 `json:"file_extension,omitempty"`
+	DefaultBhvReport interface{}            `json:"default_behavior_report,omitempty"`
+	BhvScans         interface{}            `json:"behavior_scans,omitempty"`
+	Status           int                    `json:"status,omitempty"`
 }
 
 // Submission represents a file submission.
