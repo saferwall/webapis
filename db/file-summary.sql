778e5d85affd490811b39e6e5a0b3ca08dbb47a1
Noteworthy <lord.noteworthy@protonmail.com>
2024-10-13 10:23:20 +1100

    feat: improve file summary and remove `liked` (#95)

--- db/file-summary.sql
+++ db/file-summary.sql
@@ -1,12 +1,5 @@
 /* N1QL query to retrieve file summary of a scan. */
-WITH
-  user_likes AS (
-    SELECT
-      RAW ARRAY like_item.sha256 FOR like_item IN u.`likes` END AS sha256_array
-    FROM
-      `bucket_name` u
-    USE KEYS $loggedInUser
-  )
+
 SELECT
   {
     "properties": {
@@ -44,10 +37,7 @@ SELECT
     },
     "pe_meta": f.pe.meta,
     "default_behavior_report": f.default_behavior_report,
-    "liked": CASE
-      WHEN ARRAY_LENGTH(user_likes) = 0 THEN false
-      ELSE ARRAY_BINARY_SEARCH(ARRAY_SORT((user_likes) [0]), f.sha256) >= 0
-    END
+    "liked": FALSE
   }.*
 FROM
   `bucket_name` f
