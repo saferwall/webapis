880cc9ded8d61b8c71ccc5a5a9579f20a0e875ac
LordNoteworthy <lord.noteworthy@protonmail.com>
2024-10-19 19:33:14 +1100

    include meta in user, comment and behavior resources

--- internal/entity/behavior.go
+++ internal/entity/behavior.go
@@ -6,18 +6,19 @@ package entity
 
 // Behavior represents a dynamic file scan report.
 type Behavior struct {
-	Type             string      `json:"type,omitempty"`
-	SHA256           string      `json:"sha256,omitempty"`
-	Timestamp        int64       `json:"timestamp,omitempty"`
-	Environment      interface{} `json:"env,omitempty"`
-	APITrace         interface{} `json:"api_trace,omitempty"`
-	Artifacts        interface{} `json:"artifacts,omitempty"`
-	SystemEvents     interface{} `json:"sys_events,omitempty"`
-	ProcessTree      interface{} `json:"proc_tree,omitempty"`
-	Capabilities     interface{} `json:"capabilities,omitempty"`
-	ScreenshotsCount int         `json:"screenshots_count,omitempty"`
-	ScanConfig       interface{} `json:"scan_cfg,omitempty"`
-	SandboxLog       interface{} `json:"sandbox_log,omitempty"`
-	AgentLog         interface{} `json:"agent_log,omitempty"`
-	Status           int         `json:"status,omitempty"`
+	Meta             *DocMetadata `json:"doc,omitempty"`
+	Type             string       `json:"type,omitempty"`
+	SHA256           string       `json:"sha256,omitempty"`
+	Timestamp        int64        `json:"timestamp,omitempty"`
+	Environment      interface{}  `json:"env,omitempty"`
+	APITrace         interface{}  `json:"api_trace,omitempty"`
+	Artifacts        interface{}  `json:"artifacts,omitempty"`
+	SystemEvents     interface{}  `json:"sys_events,omitempty"`
+	ProcessTree      interface{}  `json:"proc_tree,omitempty"`
+	Capabilities     interface{}  `json:"capabilities,omitempty"`
+	ScreenshotsCount int          `json:"screenshots_count,omitempty"`
+	ScanConfig       interface{}  `json:"scan_cfg,omitempty"`
+	SandboxLog       interface{}  `json:"sandbox_log,omitempty"`
+	AgentLog         interface{}  `json:"agent_log,omitempty"`
+	Status           int          `json:"status,omitempty"`
 }
