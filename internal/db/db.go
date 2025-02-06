a95402351111c3ff22ddd9e78e1a97bc6e540897
JALAL <eabduljalil@outlook.com>
2025-02-06 11:29:52 +0100

    fix config in db.go

--- internal/db/db.go
+++ internal/db/db.go
@@ -262,46 +262,46 @@ func (db *DB) Search(ctx context.Context, stringQuery string, val *interface{}, 
 				Type: gen.NUMBER,
 			},
 			"avast": {
-				Path: "multiav.last_scan.avast.output",
+				Field: "multiav.last_scan.avast.output",
 			},
 			"avira": {
-				Path: "multiav.last_scan.avira.output",
+				Field: "multiav.last_scan.avira.output",
 			},
 			"bitdefender": {
-				Path: "multiav.last_scan.bitdefender.output",
+				Field: "multiav.last_scan.bitdefender.output",
 			},
 			"clamav": {
-				Path: "multiav.last_scan.clamav.output",
+				Field: "multiav.last_scan.clamav.output",
 			},
 			"comodo": {
-				Path: "multiav.last_scan.comodo.output",
+				Field: "multiav.last_scan.comodo.output",
 			},
 			"drweb": {
-				Path: "multiav.last_scan.drweb.output",
+				Field: "multiav.last_scan.drweb.output",
 			},
 			"eset": {
-				Path: "multiav.last_scan.eset.output",
+				Field: "multiav.last_scan.eset.output",
 			},
 			"kaspersky": {
-				Path: "multiav.last_scan.kaspersky.output",
+				Field: "multiav.last_scan.kaspersky.output",
 			},
 			"mcafee": {
-				Path: "multiav.last_scan.mcafee.output",
+				Field: "multiav.last_scan.mcafee.output",
 			},
 			"sophos": {
-				Path: "multiav.last_scan.sophos.output",
+				Field: "multiav.last_scan.sophos.output",
 			},
 			"symantec": {
-				Path: "multiav.last_scan.symantec.output",
+				Field: "multiav.last_scan.symantec.output",
 			},
 			"trendmicro": {
-				Path: "multiav.last_scan.trendmicro.output",
+				Field: "multiav.last_scan.trendmicro.output",
 			},
 			"windefender": {
-				Path: "multiav.last_scan.windefender.output",
+				Field: "multiav.last_scan.windefender.output",
 			},
 			"fsecure": {
-				Path: "multiav.last_scan.fsecure.output",
+				Field: "multiav.last_scan.fsecure.output",
 			},
 		},
 	)
